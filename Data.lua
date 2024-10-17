d5837f1103bbe909c6bd1adc2fdc8ceabac20cdf
PPPlflflaokrkwemmf78VHJkjeu3737373hNJKr <exitlaghostbrroblox@gmail.com>
2024-10-16 22:21:14 -0300

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -1,5 +1,6 @@
 local Data = {
   { key = "AtlasDev_029424", hwid = "D6887D26-02FE-4D04-83DE-C1389C14ACB8", date = "2024-14-10 13:51" }, --Perm Acces.
   { key = "AtlasDev_PMDJ32", hwid = "04c8ffc2-29a0-4ca7-8ddf-aeb8b3668433", date = "2024-14-10 14:51" }, -- 882814632488747078 Perm Access
+  { key = "AtlasDev_8MMKW9", hwid = "FE44D3BF-F19F-4600-A41B-E2C0CB458FBA", date = "2024-14-10 14:51" },-- 675846142545756202 16-10
   }
 return Data
