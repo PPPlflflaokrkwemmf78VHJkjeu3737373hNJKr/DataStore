f74fc08b03f22137960fdda0b6d89364e891a420
PPPlflflaokrkwemmf78VHJkjeu3737373hNJKr <exitlaghostbrroblox@gmail.com>
2024-10-19 19:28:50 -0300

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -2,8 +2,8 @@ local Data = {
   { key = "AtlasDev_029424", hwid = "D6887D26-02FE-4D04-83DE-C1389C14ACB8", date = "2024-14-10 13:51" }, --Perm Acces.
   { key = "AtlasDev_PMDJ32", hwid = "04c8ffc2-29a0-4ca7-8ddf-aeb8b3668433", date = "2024-14-10 14:51" }, -- 882814632488747078  Perm Access
   { key = "AtlasDev_8MMKW9", hwid = "FE44D3BF-F19F-4600-A41B-E2C0CB458FBA", date = "2024-14-10 14:51" }, -- 675846142545756202  23-10 
-  { key = "AtlasDev_P436NF", hwid = "1de62d29-d20a-4438-9d49-4d8b19de2b95", date = "2024-14-10 14:51" }, -- 1201757307743244325 26-10
-  { key = "AtlasDev_P436QF", hwid = "1F29C5D7-AB5C-4DA5-8DC1-88F3093DF2AA", date = "2024-14-10 14:51" },  -- 1201757307743244325 26-10
+  { key = "AtlasDev_P436NF", hwid = "1de62d29-d20a-4438-9d49-4d8b19de2b95", date = "2024-14-10 14:51" }, -- 1201757307743244325 29-10
+  { key = "AtlasDev_P436QF", hwid = "1F29C5D7-AB5C-4DA5-8DC1-88F3093DF2AA", date = "2024-14-10 14:51" },  -- 1201757307743244325 29-10
   { key = "AtlasDev_57JSKR", hwid = "DBABB700-5A4D-41CF-A13C-D211DC791283", date = "2024-14-10 14:51" },  -- 1114529949806759977 24/10
   { key = "AtlasDev_84JKMA", hwid = "6a7f3779-292a-4a8c-ba7f-422851f82894", date = "2024-14-10 14:51" },  -- 883700856434880545 24/10
   { key = "AtlasDev_PLDW42", hwid = "907f8b49-c956-4d2b-97ac-4bb8ab016ee2", date = "2024-14-10 14:51" },  -- 1145750831174537276 24/10
