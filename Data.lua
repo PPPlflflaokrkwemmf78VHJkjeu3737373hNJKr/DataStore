72fe41a2129f60f03975b1feec67de202b8998a6
PPPlflflaokrkwemmf78VHJkjeu3737373hNJKr <exitlaghostbrroblox@gmail.com>
2024-10-24 19:13:48 -0300

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -2,18 +2,10 @@ local Data = {
   { key = "AtlasDev_029424", hwid = "D6887D26-02FE-4D04-83DE-C1389C14ACB8", date = "2024-14-10 13:51" }, --Perm Acces.
   { key = "AtlasDev_PMDJ32", hwid = "04c8ffc2-29a0-4ca7-8ddf-aeb8b3668433", date = "2024-14-10 14:51" }, -- 882814632488747078  Perm Access
   { key = "AtlasDev_S3TGJ3", hwid = "f6f0c5db-e1be-4d77-ba80-80997b9b545e", date = "2024-14-10 14:51" }, -- juanlua  showcaser
-  { key = "AtlasDev_8MMKW9", hwid = "FE44D3BF-F19F-4600-A41B-E2C0CB458FBA", date = "2024-14-10 14:51" }, -- 675846142545756202  23-10 
   { key = "AtlasDev_P436NF", hwid = "1de62d29-d20a-4438-9d49-4d8b19de2b95", date = "2024-14-10 14:51" }, -- 1201757307743244325 29-10
   { key = "AtlasDev_P436QF", hwid = "1F29C5D7-AB5C-4DA5-8DC1-88F3093DF2AA", date = "2024-14-10 14:51" },  -- 1201757307743244325 29-10
-  { key = "AtlasDev_57JSKR", hwid = "DBABB700-5A4D-41CF-A13C-D211DC791283", date = "2024-14-10 14:51" },  -- 1114529949806759977 24/10
-  { key = "AtlasDev_84JKMA", hwid = "6a7f3779-292a-4a8c-ba7f-422851f82894", date = "2024-14-10 14:51" },  -- 883700856434880545 24/10
-  { key = "AtlasDev_PLDW42", hwid = "907f8b49-c956-4d2b-97ac-4bb8ab016ee2", date = "2024-14-10 14:51" },  -- 1145750831174537276 24/10
   { key = "AtlasDev_84JS42", hwid = "C0E71E7C-EBFB-4E82-9E0A-8FD97B87F625", date = "2024-14-10 14:51" },  -- 1214585350425411616 27/10
-  { key = "AtlasDev_o39g7D", hwid = "32f0a4bb-81cc-404c-a1c6-0b6d4bcb3a9c", date = "2024-14-10 14:51" },  -- 1248618917472239719 24/10
-  { key = "AtlasDev_43mfsD", hwid = "a36b0e6a-21c8-4df9-9075-9992a363fc24", date = "2024-14-10 14:51" },  -- 1136246659236495420 24/10
-  { key = "AtlasDev_MFN12F", hwid = "78daa372-4c82-4f21-8e25-d34a192c234f", date = "2024-14-10 14:51" },  -- 1273715597326291004 24/10
   { key = "AtlasDev_43GBDS", hwid = "4B4B79D4-5E2F-42A2-87EF-970658071FDD", date = "2024-14-10 14:51" },  -- 1168060443558948955 27/10
-  { key = "AtlasDev_z89y4j", hwid = "be06e2b2-da8a-4e83-9a5f-05636b581fa5", date = "2024-14-10 14:51" },  -- 1020664756190838836 24/10
   { key = "AtlasDev_M4PAM3", hwid = "9bb32248-59b3-455d-8f1f-9e68d8931161", date = "2024-14-10 14:51" },  -- 1078725501855740065 25/10
   { key = "AtlasDev_03nfka", hwid = "506254A9-64B1-474F-AB3E-59EA56964DDC", date = "2024-14-10 14:51" },  -- 1168143180290588683 25/10
   { key = "AtlasDev_VHUH4a", hwid = "37d42430-e6e9-426f-b7ed-598ecc29c28b", date = "2024-14-10 14:51" },  -- 1193987432413143150 25/10
