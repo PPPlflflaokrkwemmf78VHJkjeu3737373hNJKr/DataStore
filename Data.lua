b8e7a1988389bd35c2571874badf3504061ee264
PPPlflflaokrkwemmf78VHJkjeu3737373hNJKr <exitlaghostbrroblox@gmail.com>
2024-10-27 23:17:17 -0300

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -4,9 +4,6 @@ local Data = {
   { key = "AtlasDev_S3TGJ3", hwid = "f6f0c5db-e1be-4d77-ba80-80997b9b545e", date = "2024-14-10 14:51" }, -- juanlua  showcaser
   { key = "AtlasDev_P436NF", hwid = "1de62d29-d20a-4438-9d49-4d8b19de2b95", date = "2024-14-10 14:51" }, -- 1201757307743244325 29-10
   { key = "AtlasDev_P436QF", hwid = "1F29C5D7-AB5C-4DA5-8DC1-88F3093DF2AA", date = "2024-14-10 14:51" },  -- 1201757307743244325 29-10
-  { key = "AtlasDev_84JS42", hwid = "C0E71E7C-EBFB-4E82-9E0A-8FD97B87F625", date = "2024-14-10 14:51" },  -- 1214585350425411616 27/10
-  { key = "AtlasDev_43GBDS", hwid = "4B4B79D4-5E2F-42A2-87EF-970658071FDD", date = "2024-14-10 14:51" },  -- 1168060443558948955 27/10
-  { key = "AtlasDev_43GBD1", hwid = "2BDD44C9-845D-4ED3-B349-F0AD9F30C372", date = "2024-14-10 14:51" },  -- 1048997633886277724 27/10
   { key = "AtlasDev_B4NSJQ", hwid = "8d0fa7e8-e638-4897-9f0a-f076b902e5de", date = "2024-14-10 14:51" },  -- 838422662895108126 29/10
   }
 return Data
