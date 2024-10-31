b3a6c93742d684be102940f2f5defecd6b59ae2a
PPPlflflaokrkwemmf78VHJkjeu3737373hNJKr <exitlaghostbrroblox@gmail.com>
2024-10-31 00:26:59 -0300

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -1,6 +1,7 @@
 local Data = {
   { key = "AtlasDev_029424", hwid = "D6887D26-02FE-4D04-83DE-C1389C14ACB8", date = "2024-14-10 13:51" }, --Perm Acces.
   { key = "AtlasDev_PMDJ32", hwid = "04c8ffc2-29a0-4ca7-8ddf-aeb8b3668433", date = "2024-14-10 14:51" }, -- 882814632488747078  Perm Access
-  { key = "AtlasDev_S3TGJ3", hwid = "f6f0c5db-e1be-4d77-ba80-80997b9b545e", date = "2024-14-10 14:51" }, -- juanlua  showcaser
-  }
+  { key = "AtlasDev_S3TGJ3", hwid = "f6f0c5db-e1be-4d77-ba80-80997b9b545e", date = "2024-14-10 14:51" }, -- juanlua  showcaser  
+  { key = "AtlasDev_iuy439", hwid = "3f80d245-4a60-4742-8c76-501fe84b1689", date = "2024-14-10 14:51" }, -- cara q me deu a conta
+  
 return Data
